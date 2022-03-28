e8d3a8c3f24dd389300478ba3218bf6647acd3ac
Luis Silva <luansimo@outlook.com>
2022-03-28 09:03:11 -0600

    Ajustes de formato a instancias de prueba

--- src/App.java
+++ src/App.java
@@ -35,36 +35,54 @@ public class App {
         instance.setDataset(instances);
         instance.setValue(0, 34);// Age
         instance.setValue(1, 0);// Sex
-        instance.setValue(2, 1);// Chest Pain [intensity]
-        instance.setValue(3, 196);// Resting Blood Pressure [mm Hg]
+        instance.setValue(2, 1);// Chest Pain
+        instance.setValue(3, 196);// Resting Blood Pressure
         instance.setValue(4, 210);// Cholesterol
-        instance.setValue(5, 0);// Fasting Blood Sugar [bool]
+        instance.setValue(5, 0);// Fasting Blood Sugar
 
         int result = (int) tree.classifyInstance(instance);
 
-        System.out.println("Instancia de prueba no. 1: " + result);
+        System.out.println("Edad: 34 años");
+        System.out.println("Sexo: Mujer");
+        System.out.println("Dolor de pecho: Angina típica");
+        System.out.println("Presión en reposo: 196 mmHg");
+        System.out.println("Colesterol: 210 mg/dl");
+        System.out.println("Azucar en sangre > 120 mg/dl: 0");
+        System.out.println("Predicción: " + result);
 
         instance.setValue(0, 68);// Age
         instance.setValue(1, 1);// Sex
-        instance.setValue(2, 3);// Chest Pain [intensity]
-        instance.setValue(3, 150);// Resting Blood Pressure [mm Hg]
+        instance.setValue(2, 3);// Chest Pain
+        instance.setValue(3, 150);// Resting Blood Pressure
         instance.setValue(4, 160);// Cholesterol
-        instance.setValue(5, 1);// Fasting Blood Sugar [bool]
+        instance.setValue(5, 1);// Fasting Blood Sugar
 
         result = (int) tree.classifyInstance(instance);
 
-        System.out.println("Instancia de prueba no. 2: " + result);
+        System.out.println("Edad: 68 años");
+        System.out.println("Sexo: Hombre");
+        System.out.println("Dolor de pecho: No angina");
+        System.out.println("Presión en reposo: 150 mmHg");
+        System.out.println("Colesterol: 160 mg/dl");
+        System.out.println("Azucar en sangre > 120 mg/dl: 1");
+        System.out.println("Predicción: " + result);
 
-        instance.setValue(0, 34);// Age
+        instance.setValue(0, 37);// Age
         instance.setValue(1, 1);// Sex
-        instance.setValue(2, 1);// Chest Pain [intensity]
-        instance.setValue(3, 196);// Resting Blood Pressure [mm Hg]
+        instance.setValue(2, 1);// Chest Pain
+        instance.setValue(3, 196);// Resting Blood Pressure
         instance.setValue(4, 170);// Cholesterol
-        instance.setValue(5, 0);// Fasting Blood Sugar [bool]
+        instance.setValue(5, 0);// Fasting Blood Sugar
 
         result = (int) tree.classifyInstance(instance);
 
-        System.out.println("Instancia de prueba no. 3: " + result);
+        System.out.println("Edad: 37 años");
+        System.out.println("Sexo: Hombre");
+        System.out.println("Dolor de pecho: Angina típica");
+        System.out.println("Presión en reposo: 196 mmHg");
+        System.out.println("Colesterol: 170 mg/dl");
+        System.out.println("Azucar en sangre > 120 mg/dl: 0");
+        System.out.println("Predicción: " + result);
 
 
     }
